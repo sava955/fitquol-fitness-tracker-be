1c24cd09316e76a4bfe77793ee431f32af64e0e9
sava995 <kostadinoviccc@gmail.com>
2025-05-16 22:51:57 +0200

    update

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ import * as authCtrl from '../controllers/auth.js';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({ message: "Auth root endpoint works!" });
+  });
+
 router.post("/register", authCtrl.register);
 router.post("/login", authCtrl.login);
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,10 @@ import * as authMiddleware from '../utils/auth-middleware.js';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({ message: "Users root endpoint works!" });
+  });
+
 router.get('/current-user', authMiddleware.verifyToken, usersCtrl.getCurrentUser);
 router.patch('/:id', authMiddleware.verifyToken, usersCtrl.updateUser);
 router.patch('/password/:id', authMiddleware.verifyToken, usersCtrl.updatePassword);
