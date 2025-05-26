8a5b17f1685983b6234c08f7974189992d3df0b5
sava995 <kostadinoviccc@gmail.com>
2025-05-26 20:03:11 +0200

    update

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,6 +1,12 @@
 import fs from "fs";
 import path from "path";
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import Recipe from "../models/recipe.js";
 import RecipeCategory from "../models/recipe-category.js";
 import Goal from "../models/goal.js";
